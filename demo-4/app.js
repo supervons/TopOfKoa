9af3f17630e447e260a6916acf3977746ae3fa07
supervons <supervons@sina.com>
2021-08-10 11:15:29 +0800

    add prettier config.

--- demo-4/app.js
+++ demo-4/app.js
@@ -1,15 +1,15 @@
-const koa=require('koa');
-const Router = require('@koa/router')
-const app=new koa();
-const router = new Router()
-router.get('/', async(ctx, next)=>{
-  ctx.body = 'Hello world!'
-})
-router.get('/404', async(ctx, next)=>{
-  ctx.body = '404 - node found.'
-})
+const koa = require("koa");
+const Router = require("@koa/router");
+const app = new koa();
+const router = new Router();
+router.get("/", async (ctx, next) => {
+  ctx.body = "Hello world!";
+});
+router.get("/404", async (ctx, next) => {
+  ctx.body = "404 - node found.";
+});
 
-app.use(router.routes())
-app.listen(3000,()=>{
-  console.log('启动成功！http://localhost:3000')
+app.use(router.routes());
+app.listen(3000, () => {
+  console.log("启动成功！http://localhost:3000");
 });
